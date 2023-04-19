b35487119c955f5ddceed9daca5e615cc0828f34
Andrew Lund <andrew.s.lund@gmail.com>
2023-04-19 16:01:31 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,59 @@ CREATE DATABASE chat;
 
 USE chat;
 
+DROP TABLE IF EXISTS `messages`;
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `text` MEDIUMTEXT NULL DEFAULT NULL,
+  `id_rooms` INTEGER NOT NULL,
+  `id_users` INTEGER NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
 /* Create other tables and define schemas for them here! */
 
+DROP TABLE IF EXISTS `rooms`;
 
+CREATE TABLE `rooms` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` MEDIUMTEXT,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` MEDIUMTEXT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (id_rooms) REFERENCES `rooms` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`text`,`id_rooms`,`id_users`) VALUES
+-- ('','','','');
+-- INSERT INTO `rooms` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `users` (`id`,`name`) VALUES
+-- ('','');
 
 
 /*  Execute this file from the command line by typing:
